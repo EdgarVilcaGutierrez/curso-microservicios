ecc107cd28399181460f2781d040c4f9879d95fa
edgarvilcag@gmail.com <edgarvilcag@gmail.com>
2022-12-01 21:04:20 -0500

    gateway

--- usuario-service/src/main/java/com/usuario/service/feignclients/MotoFeignClient.java
+++ usuario-service/src/main/java/com/usuario/service/feignclients/MotoFeignClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.usuario.service.modelos.Moto;
 
-@FeignClient(name="moto-service",url="http://localhost:8083")
+@FeignClient(name="moto-service")
 @RequestMapping("/moto")
 public interface MotoFeignClient {
 	
